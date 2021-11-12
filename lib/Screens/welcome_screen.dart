380e7fe345cb3668a07c1ee258718d8443d3fc36
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-11-12 19:42:28 -0300

    white menu widget added

--- lib/Screens/welcome_screen.dart
+++ lib/Screens/welcome_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:keleya_app/Widgets/rounded_buttons.dart';
+import 'package:keleya_app/routes/named_routes.dart';
+import 'package:keleya_app/widgets/white_menu.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
@@ -8,63 +10,39 @@ class WelcomeScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: Color(0xff5a4fd9),
       body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(
-                vertical: 60,
-                horizontal: 90,
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    'Are you already a Kaleya user?',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                    ),
+        child: WhiteMenu(
+          child: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RoundedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      NamedRoutes.signIn,
+                    );
+                  },
+                  title: 'LogIn',
+                  color: Color(0xff5a4fd9),
+                  style: TextStyle(color: Colors.white),
+                ),
+                RoundedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(
+                      context,
+                      NamedRoutes.signUp,
+                    );
+                  },
+                  title: 'Register',
+                  color: Color(0xfff3ecff),
+                  style: TextStyle(
+                    color: Color(0xff5a4fd9),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Container(
-                padding: EdgeInsets.symmetric(
-                  horizontal: 20,
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      RoundedButton(
-                        onPressed: null,
-                        title: 'LogIn',
-                        color: Color(0xff5a4fd9),
-                        style: TextStyle(color: Colors.white),
-                      ),
-                      RoundedButton(
-                        onPressed: null,
-                        title: 'Register',
-                        color: Color(0xfff3ecff),
-                        style: TextStyle(
-                          color: Color(0xff5a4fd9),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
