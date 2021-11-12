967eba23f44f65f6c16e5c6f51d1a7ece066a53d
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-11-12 18:00:45 -0300

    all routes added

--- lib/routes/named_routes.dart
+++ lib/routes/named_routes.dart
@@ -1,3 +1,8 @@
 class NamedRoutes {
   static const home = '/';
+  static const signIn = 'SignInScreen';
+  static const signUp = 'SignUpScreen';
+  static const name = 'NameScreen';
+  static const date = 'DateScreen';
+  static const success = 'SuccessScreen';
 }
