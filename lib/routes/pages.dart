71c9bc4e7897c08aa44d8ab9d899120ff31201b6
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-11-12 19:37:38 -0300

    welcome screen routes to go to signIn and signOut added

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:keleya_app/Screens/welcome_screen.dart';
 import 'package:keleya_app/routes/named_routes.dart';
+import 'package:keleya_app/screens/sign_in_screen.dart';
+import 'package:keleya_app/screens/sign_up_screen.dart';
 
 class Pages {
   static Map<String, WidgetBuilder> all(BuildContext context) {
     return {
       NamedRoutes.home: (context) => WelcomeScreen(),
+      NamedRoutes.signIn: (context) => SignInScreen(),
+      NamedRoutes.signUp: (context) => SignUpScreen(),
     };
   }
 }
