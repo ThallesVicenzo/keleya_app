cfa3e56b71f5da85ec8b2a8cfe66ab1a6233883c
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-11-16 22:32:02 -0300

    constants file with  kTextFieldDec added

---
+++ lib/constants.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const kTextFieldDecoration = InputDecoration(
+  hintStyle: TextStyle(color: Colors.grey),
+  contentPadding:
+  EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.grey, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Color(0xff5a4fd9), width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  ),
+);
