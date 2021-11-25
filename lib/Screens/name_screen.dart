69e6c2cee743775fdb5c2227d63897564a0326e9
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-11-24 22:55:31 -0300

    name screen with all its components added

--- lib/Screens/name_screen.dart
+++ lib/Screens/name_screen.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:keleya_app/constants.dart';
-import 'package:keleya_app/routes/pages.dart';
+import 'package:keleya_app/routes/named_routes.dart';
+import 'package:keleya_app/widgets/rounded_buttons.dart';
+import 'package:keleya_app/widgets/title_widget.dart';
 
-class NameScreen extends StatelessWidget {
+class NameScreen extends StatefulWidget {
+  @override
+  _NameScreenState createState() => _NameScreenState();
+}
+
+class _NameScreenState extends State<NameScreen> {
+  String name;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: kThemeColor,
-      body: Column(
-        children: [
-          Text('teste'),
-        ],
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            TitleWidget(
+              title: 'What should we call you?',
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onChanged: (value) {
+                  name = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Type you nickname here',
+                ),
+              ),
+            ),
+            RoundedButton(
+              title: 'Next question',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+              color: kThemeColor,
+              onPressed: (){
+                setState(() {
+                  Navigator.pushNamed(context, NamedRoutes.date);
+                },);
+              }
+            ),
+          ],
+        ),
       ),
     );
   }
