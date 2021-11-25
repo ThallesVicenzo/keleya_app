b8d05c2f800246bdcc03d0ff95938f791a74f99d
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-11-24 22:56:20 -0300

    sucess screen finished

--- lib/screens/success_screen.dart
+++ lib/screens/success_screen.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:keleya_app/widgets/title_widget.dart';
 
 class SuccessScreen extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            TitleWidget(title: 'Congratulations on your new arrival!',),
+            Image.asset('assets/images/baby.png'),
+          ],
+        ),
+      ),
+    );
   }
 }
