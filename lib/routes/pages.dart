0c39546d9c7d02c236e9334949fea87f38901d71
ThallesVicenzo <77857665+ThallesVicenzo@users.noreply.github.com>
2021-11-24 22:55:01 -0300

    date screen route added

--- lib/routes/pages.dart
+++ lib/routes/pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:keleya_app/Screens/welcome_screen.dart';
 import 'package:keleya_app/routes/named_routes.dart';
+import 'package:keleya_app/screens/date_screen.dart';
 import 'package:keleya_app/screens/sign_in_screen.dart';
 import 'package:keleya_app/screens/sign_up_screen.dart';
 import 'package:keleya_app/screens/success_screen.dart';
@@ -14,6 +15,7 @@ class Pages {
       NamedRoutes.signUp: (context) => SignUpScreen(),
       NamedRoutes.success: (context) => SuccessScreen(),
       NamedRoutes.name: (context) => NameScreen(),
+      NamedRoutes.date: (context) => DateScreen(),
     };
   }
 }
